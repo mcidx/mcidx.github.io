69829df67bf301a987c7a93bae5212a9c552cfd4
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-07-22 20:50:05 -0500

    more static generation

--- src/scripts/setupComponents.ts
+++ src/scripts/setupComponents.ts
@@ -1,8 +1,13 @@
-import { mkdir } from "node:fs/promises";
+import { camelCase, upperFirst } from "lodash-es";
+import { mkdir, rm, writeFile } from "node:fs/promises";
+import { format } from "prettier";
+import { type LanguageName } from "quicktype-core";
+import { unqualify } from "../game/unqualify";
 import { vfs } from "../game/vfs";
 
 const readRoot = "report/generated/reports/minecraft/components/item";
 const writeRoot = "src/generated/components";
+const targetLanguage: LanguageName = "typescript-zod";
 const items = await vfs.dir(readRoot);
 
 const observations = new Map<string, unknown[]>();
@@ -22,8 +27,73 @@ for (const item of items) {
   }
 }
 
+await rm(writeRoot, { recursive: true });
 await mkdir(writeRoot, { recursive: true });
 
-for (const [name, samples] of observations) {
-  break;
+let i = 0;
+for (const [id, samples] of observations) {
+  if (i++ === 3) break;
+
+  const unqualified = unqualify(id);
+  const name = upperFirst(camelCase(unqualified));
+
+  const path = `${writeRoot}/${unqualified}.ts`;
+  const schema = inferSchema(samples[0]);
+
+  let content = "";
+
+  content += 'import { z } from "zod";\n\n';
+  content += `export const ${name} = `;
+  content += schema;
+  content += ";\n";
+
+  content = await format(content, { parser: "typescript" });
+
+  await writeFile(path, content);
+}
+
+function inferSchema(sample: unknown) {
+  const type = typeof sample;
+
+  if (type === "string") {
+    return "z.string()";
+  }
+
+  if (type === "number") {
+    return "z.number()";
+  }
+
+  if (type === "boolean") {
+    return "z.boolean()";
+  }
+
+  if (type === "object") {
+    if (sample === null) {
+      return "z.null()";
+    }
+
+    if (Array.isArray(sample)) {
+      let child = "z.never()";
+
+      if (sample.length > 0) {
+        child = inferSchema(sample[0]);
+      }
+
+      return `z.array(${child})`;
+    }
+
+    let draft = "";
+
+    draft += "z.object({";
+
+    for (const key in sample) {
+      draft += `${key}:${inferSchema((sample as Record<string, unknown>)[key])},`;
+    }
+
+    draft += "})";
+
+    return draft;
+  }
+
+  throw new Error(`Unhandled type ${type}`);
 }
